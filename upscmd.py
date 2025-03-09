1e26e626db61a26a0962639c8eb64f5bb184abfc
Matteo Crocetti <75637780+cocciasecca@users.noreply.github.com>
2025-03-09 13:28:33 +0100

    Update upscmd.py
    Updated upscmd.py to work with python3 instead of python2

--- upscmd.py
+++ upscmd.py
@@ -1,38 +1,44 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import sys
 import telnetlib
 
-user = "<upsd_username>"
-pwd = "<upsd_pwd>"
+# Credentials configuration
+USER = "<upsd_username>"
+PWD = "<upsd_pwd>"
 
-if len(sys.argv) == 2:
-    cmd = sys.argv[1]
-else:
-    print("the ups command to issue is missing.")
-    print("example: upscmd.py beeper.enable")
+if len(sys.argv) != 2:
+    print("Errore: Comando UPS mancante.")
+    print("Esempio: upscmd.py beeper.enable")
     exit(1)
 
-tn = telnetlib.Telnet("127.0.0.1", 3493)
+cmd = sys.argv[1]
 
-tn.write("USERNAME {0}\n".format(user))
-response = tn.read_until("OK", timeout=2)
-print("USERNAME: {0}".format(response.strip()))
+try:
+    tn = telnetlib.Telnet("127.0.0.1", 3493)
+    
+    tn.write(f"USERNAME {USER}\n".encode("utf-8"))
+    response = tn.read_until(b"OK", timeout=2).decode("utf-8").strip()
+    print(f"USERNAME: {response}")
 
-tn.write("PASSWORD {0}\n".format(pwd))
-response = tn.read_until("OK", timeout=2)
-print("PASSWORD: {0}".format(response.strip()))
+    tn.write(f"PASSWORD {PWD}\n".encode("utf-8"))
+    response = tn.read_until(b"OK", timeout=2).decode("utf-8").strip()
+    print(f"PASSWORD: {response}")
 
-tn.write("INSTCMD ups {0}\n".format(cmd))
-response = tn.read_until("OK", timeout=2)
-print("INSTCMD ups {0}: {1}".format(cmd, response.strip()))
+    tn.write(f"INSTCMD ups {cmd}\n".encode("utf-8"))
+    response = tn.read_until(b"OK", timeout=2).decode("utf-8").strip()
+    print(f"INSTCMD ups {cmd}: {response}")
 
-if response.strip() != "OK":
-  tn.write("LIST CMD ups\n")
-  response = tn.read_until("END LIST CMD ups", timeout=2)
-  print("\n>> AVAILABLE CMDS:")
-  cmds = response.splitlines()[1:-1]
-  for cmd in cmds:
-    print(cmd.replace("CMD ups ", "- "))
+    if response != "OK":
+        tn.write(b"LIST CMD ups\n")
+        response = tn.read_until(b"END LIST CMD ups", timeout=2).decode("utf-8")
+        print("\n>> AVAILABLE CMDS:")
+        for line in response.splitlines()[1:-1]:
+            print("- " + line.replace("CMD ups ", ""))
 
-tn.write("LOGOUT\n")
-print tn.read_all().rstrip("\n")
+    tn.write(b"LOGOUT\n")
+    print(tn.read_all().decode("utf-8").strip())
+    tn.close()
+
+except Exception as e:
+    print(f"Errore: {e}")
+    exit(1)
